e68137b3efca882b9d7176afbd9582164223834a
Elvis Oliveira <elvis.olv@gmail.com>
2022-09-01 10:35:08 +0200

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,1 +1,10 @@
 set fish_key_bindings fish_vi_key_bindings
+
+# gv stands for grep view
+function gv
+    # usage: grep -rn "regex" | gv
+    # requires:
+    # https://github.com/junegunn/fzf
+    # https://github.com/sharkdp/bat
+    fzf --preview='bat --theme=Dracula --color=always {1} --highlight-line {2}' --delimiter=':' --ansi --color "hl:-1:underline,hl+:-1:underline:reverse" --preview-window 'up,60%,border-bottom,+{2}+3/3,~3'
+end
