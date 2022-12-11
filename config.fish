2dd7f91b00deb9024d3c0ec6b2ab1bd85afe3d93
Elvis Oliveira <elvis.olv@gmail.com>
2022-12-11 15:52:40 +0100

    Set prompt info on the right side

--- config.fish
+++ config.fish
@@ -6,11 +6,12 @@ fish_vi_key_bindings
 
 set -Ux PYENV_ROOT $HOME/.pyenv
 set -Ux EDITOR nvim
+set -Ux VIRTUAL_ENV_DISABLE_PROMPT 1
 
 alias ls="ls --color=always"
 alias vi="nvim"
-alias grep="grep -rn"
-alias scrcpy="scrcpy --stay-awake --turn-screen-off --max-size 800 --bit-rate 2M --hid-keyboard"
+alias grep="grep -n --exclude-dir={node_modules,vendor,.git,dll,tests}"
+alias scrcpy="scrcpy --stay-awake --turn-screen-off --max-size 800 --bit-rate 2M --hid-keyboard --serial ZF5236LMZK"
 
 function c
     cat $argv ^/dev/null | tr -d '\n' | read -l input
@@ -25,29 +26,25 @@ end
 # end
 
 function fish_prompt
+    # echo -n (whoami)'@'(hostname)':'
+    echo -n (set_color $fish_color_cwd)(prompt_pwd) '$ '
+end
+
+function fish_right_prompt
+    echo -n -s ' ' (php -r 'echo PHP_VERSION;')
+
     if set -q VIRTUAL_ENV
-        echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+        echo -n -s  '  ' (echo -e "import platform\nprint(platform.python_version())" | python)
     end
 
-    set_color normal
-    echo -n (whoami)'@'(hostname)':'
-
-    set_color $fish_color_cwd
-    echo -n (prompt_pwd)
+    echo -n -s '  ' (node -v)
 
     # https://stackoverflow.com/questions/24581793/ps1-prompt-in-fish-friendly-interactive-shell-show-git-branch
     set -l GIT_BRANCH (git branch 2>/dev/null | sed -n '/\* /s///p')
-    if test -n "$GIT_BRANCH"
-        set_color normal
-        echo -n '['
-        set_color purple
-        echo -n "$GIT_BRANCH"
-        set_color normal
-        echo -n ']'
-    end
 
-    set_color normal
-    echo -n ' $ '
+    if test -n "$GIT_BRANCH"
+        echo -n -s  ' ' (set_color normal) '[' (set_color purple) $GIT_BRANCH (set_color normal) ']'
+    end
 end
 
 if [ -f ~/.phpbrew/phpbrew.fish ]
@@ -71,12 +68,13 @@ function gv
     # https://github.com/sharkdp/bat
     fzf --preview='fzf-bat-preview {1} {2}' \
         --delimiter=':' \
+        --no-mouse \
         --ansi \
         --color "hl:-1:underline,hl+:-1:underline:reverse" | read stdout
     set file (echo $stdout | cut -f1 -d":")
     set line (echo $stdout | cut -f2 -d":")
     if test -n "$file"
-        vim +"$line" $file -c 'normal zz' < /dev/tty
+        vi +"$line" $file -c 'normal zz' < /dev/tty
     end
 end
 
